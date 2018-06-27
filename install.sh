3a95b392b6dcf7693f2eb968af71faa776923e95
Benjamin Hillman <benjaminr.hillman@gmail.com>
2018-06-27 16:01:03 -0600

    Fix copying of old files for backup

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+git submodule update --init
+
 dotfiles=(.vimrc .vim .screenrc)
+
+echo "Backup existing files..."
 for file in ${dotfiles[@]}; do
     if [ -e ${HOME}/${file} ]; then
-        mv ${HOME}/${file} ${HOME}/${file}~
+        #echo "${HOME}/${file} (do nothing)"
+        mv -v ${HOME}/${file} ${HOME}/${file}.backup
     fi
-    ln -sf ${PWD}/${file} ${HOME}/
+done
+
+echo "Create symbolic links..."
+for file in ${dotfiles[@]}; do
+    ln -svf ${PWD}/${file} ${HOME}/${file}
 done
